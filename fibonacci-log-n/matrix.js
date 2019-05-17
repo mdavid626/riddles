4c8c264af36651d8d03c56739741aae2f433c762
Dávid Molnár <mdavid626@gmail.com>
2019-05-18 00:08:04 +0200

    Simplify return

--- fibonacci-log-n/matrix.js
+++ fibonacci-log-n/matrix.js
@@ -1,15 +1,13 @@
-const multiplyMatrix = (matrix1, matrix2) => {
-  return [
-    [
-      matrix1[0][0] * matrix2[0][0] + matrix1[0][1] * matrix2[1][0],
-      matrix1[0][0] * matrix2[0][1] + matrix1[0][1] * matrix2[1][1],
-    ],
-    [
-      matrix1[1][0] * matrix2[0][0] + matrix1[1][1] * matrix2[1][0],
-      matrix1[1][0] * matrix2[0][1] + matrix1[1][1] * matrix2[1][1],
-    ],
-  ];
-};
+const multiplyMatrix = (matrix1, matrix2) => [
+  [
+    matrix1[0][0] * matrix2[0][0] + matrix1[0][1] * matrix2[1][0],
+    matrix1[0][0] * matrix2[0][1] + matrix1[0][1] * matrix2[1][1],
+  ],
+  [
+    matrix1[1][0] * matrix2[0][0] + matrix1[1][1] * matrix2[1][0],
+    matrix1[1][0] * matrix2[0][1] + matrix1[1][1] * matrix2[1][1],
+  ],
+];
 
 module.exports = {
   multiplyMatrix,
