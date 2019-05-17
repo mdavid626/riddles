d8b89b26209ab64633be5ff26a1b66ce816c04ab
Dávid Molnár <mdavid626@gmail.com>
2019-05-17 23:52:43 +0200

    Replace functions with arrow functions

--- fibonacci-log-n/utils.spec.js
+++ fibonacci-log-n/utils.spec.js
@@ -4,19 +4,19 @@ const { runNTimes } = require('./utils');
 
 const pow2 = n => n * n;
 
-describe('utils', function() {
-  describe('runNTimes', function() {
-    it('should run fn 0 times', function() {
+describe('utils', () => {
+  describe('runNTimes', () => {
+    it('should run fn 0 times', () => {
       const result = runNTimes(pow2, 0, 123);
       expect(result).to.be.equal(123);
     });
 
-    it('should run fn 1 times', function() {
+    it('should run fn 1 times', () => {
       const result = runNTimes(pow2, 1, 2);
       expect(result).to.be.equal(4);
     });
 
-    it('should run fn 3 times', function() {
+    it('should run fn 3 times', () => {
       const result = runNTimes(pow2, 3, 2);
       expect(result).to.be.equal(256);
     });
