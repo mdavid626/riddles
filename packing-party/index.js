c2e6c8fd38ad13982c6c4ed08c1cbae23768362d
Dávid Molnár <david.molnar@check24.de>
2019-06-18 15:11:22 +0200

    Add basic working fp packing party solution

--- packing-party/index.js
+++ packing-party/index.js
@@ -1,5 +1,25 @@
 const distribute = (host, vms) => {
-  return [];
+  return vms.reduce((acc, vm) => {
+    const lastHost = acc[acc.length - 1] || [];
+    const current = [...lastHost, vm].reduce(
+      (acc, vm) => ({
+        cpu: (acc.cpu || 0) + vm.cpu,
+        ram: (acc.ram || 0) + vm.ram,
+        hdd: (acc.hdd || 0) + vm.hdd,
+      }),
+      {}
+    );
+    const fits =
+      current.cpu <= host.cpu &&
+      current.ram <= host.ram &&
+      current.hdd <= host.hdd;
+    return fits
+      ? [
+          ...acc.filter((_, index) => index !== acc.length - 1),
+          [...lastHost, vm],
+        ]
+      : [...acc, [vm]];
+  }, []);
 };
 
 module.exports = distribute;
