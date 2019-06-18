fed6a5ef3623004f6a7cbdea55ecab47d71128be
Dávid Molnár <david.molnar@check24.de>
2019-06-18 16:47:28 +0200

    Extract fit function

--- packing-party/index.js
+++ packing-party/index.js
@@ -1,19 +1,24 @@
+const getFits = (hostSpec, vms) => {
+  const sum = vms.reduce(
+    (acc, vm) => ({
+      cpu: (acc.cpu || 0) + vm.cpu,
+      ram: (acc.ram || 0) + vm.ram,
+      hdd: (acc.hdd || 0) + vm.hdd,
+    }),
+    {}
+  );
+  return (
+    sum.cpu <= hostSpec.cpu &&
+    sum.ram <= hostSpec.ram &&
+    sum.hdd <= hostSpec.hdd
+  );
+};
+
 const distribute = (hostSpec, vms) =>
   vms.reduce((distribution, vm) => {
     const lastHost = distribution[distribution.length - 1] || [];
     const possibleHost = [...lastHost, vm];
-    const sum = possibleHost.reduce(
-      (acc, item) => ({
-        cpu: (acc.cpu || 0) + item.cpu,
-        ram: (acc.ram || 0) + item.ram,
-        hdd: (acc.hdd || 0) + item.hdd,
-      }),
-      {}
-    );
-    const fits =
-      sum.cpu <= hostSpec.cpu &&
-      sum.ram <= hostSpec.ram &&
-      sum.hdd <= hostSpec.hdd;
+    const fits = getFits(hostSpec, possibleHost);
     return [
       ...distribution.filter((_, index) => index !== distribution.length - 1),
       ...(fits ? [possibleHost] : [lastHost, [vm]]),
