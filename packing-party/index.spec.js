6cda08d460c0c1669b67e5308270fc07878eca50
Dávid Molnár <mdavid626@gmail.com>
2019-06-18 19:04:45 +0200

    Add vm name

--- packing-party/index.spec.js
+++ packing-party/index.spec.js
@@ -6,16 +6,19 @@ describe('vm distribution', () => {
   it('should distribute vms', () => {
     const host = { cpu: 3, ram: 5, hdd: 10 };
     const vms = [
-      { cpu: 1, ram: 2, hdd: 3 },
-      { cpu: 2, ram: 2, hdd: 3 },
-      { cpu: 2, ram: 2, hdd: 3 },
+      { name: 'vm1', cpu: 1, ram: 2, hdd: 3 },
+      { name: 'vm2', cpu: 2, ram: 2, hdd: 3 },
+      { name: 'vm3', cpu: 2, ram: 2, hdd: 3 },
     ];
 
     const distribution = distributeVms(host, vms);
 
     expect(distribution).to.deep.equal([
-      [{ cpu: 1, ram: 2, hdd: 3 }, { cpu: 2, ram: 2, hdd: 3 }],
-      [{ cpu: 2, ram: 2, hdd: 3 }],
+      [
+        { name: 'vm1', cpu: 1, ram: 2, hdd: 3 },
+        { name: 'vm2', cpu: 2, ram: 2, hdd: 3 },
+      ],
+      [{ name: 'vm3', cpu: 2, ram: 2, hdd: 3 }],
     ]);
   });
 });
