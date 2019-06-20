b741a2d201e56efd97f4c48205a1874c02569512
Dávid Molnár <mdavid626@gmail.com>
2019-06-20 15:17:32 +0200

    Remove last group index

--- packing-party/first-fit.js
+++ packing-party/first-fit.js
@@ -1,11 +1,10 @@
 const distribute = (host, items, doFit) =>
   items.reduce((distribution, item) => {
-    const lastGroupIndex = distribution.length - 1;
-    const lastGroup = distribution[lastGroupIndex] || [];
+    const lastGroup = distribution[distribution.length - 1] || [];
     const possibleGroup = [...lastGroup, item];
     const fit = doFit(host, possibleGroup);
     return [
-      ...distribution.filter((_, index) => index !== lastGroupIndex),
+      ...distribution.filter((_, index) => index !== distribution.length - 1),
       ...(fit ? [possibleGroup] : [lastGroup, [item]]),
     ];
   }, []);
